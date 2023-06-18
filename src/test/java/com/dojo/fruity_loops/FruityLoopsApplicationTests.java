bd9ba859b0feadca5fabe0448f52069fd217e8e5
KlediCala1 <kledicala1@gmail.com>
2023-06-18 16:23:09 +0200

    Initial commit

---
+++ src/test/java/com/dojo/fruity_loops/FruityLoopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojo.fruity_loops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruityLoopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
